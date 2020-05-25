f7f5a4cc02346fe1ec6185bd452abbd2cf2d77d7
vscalado <vitor.santos.calado@gmail.com>
2020-05-25 11:16:23 -0300

    Criado os Mentodos GET, POST e GET por ID para a tabela VAGAS

---
+++ rest/api/services/vaga_service.py
@@ -0,0 +1,28 @@
+from django.http import Http404
+
+from ..models import Vaga
+from .tecnologia_service import listar_tecnologia_id
+
+
+def listar_vagas():
+    vagas = Vaga.objects.all()
+    return vagas
+
+
+def cadastrar_vaga(vaga):
+    vaga_bd = Vaga.objects.create(titulo=vaga.titulo, descricao=vaga.descricao, salario=vaga.salario,
+                                  tipo_contratacao=vaga.tipo_contratacao, local=vaga.local,
+                                  quantidade=vaga.quantidade, contato=vaga.contato)
+    vaga_bd.save()
+    for i in vaga.tecnologias:
+        tecnologia = listar_tecnologia_id(i.id)
+        vaga_bd.tecnologias.add(tecnologia)
+
+    return vaga_bd
+
+
+def listar_vagas_id(id):
+    try:
+        return Vaga.objects.get(id=id)
+    except Vaga.DoesNotExist:
+        raise Http404
---
+++ rest/api/views/vaga_view.py
@@ -0,0 +1,38 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..services import vaga_service
+from ..serializers import vaga_serializer
+from ..entidades import vaga
+
+
+class VagaList(APIView):
+    def get(self, request, format=None):
+        vagas = vaga_service.listar_vagas()
+        serializer = vaga_serializer.VagaSerializer(vagas, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = vaga_serializer.VagaSerializer(data=request.data)
+        if serializer.is_valid():
+            titulo = serializer.validated_data["titulo"]
+            descricao = serializer.validated_data["descricao"]
+            salario = serializer.validated_data["salario"]
+            tipo_contratacao = serializer.validated_data["tipo_contratacao"]
+            local = serializer.validated_data["local"]
+            quantidade = serializer.validated_data["quantidade"]
+            contato = serializer.validated_data["contato"]
+            tecnologias = serializer.validated_data["tecnologias"]
+            vaga_nova = vaga.Vaga(titulo=titulo, descricao=descricao, salario=salario,
+                                  tipo_contratacao=tipo_contratacao, local=local, quantidade=quantidade,
+                                  contato=contato, tecnologias=tecnologias)
+            vaga_service.cadastrar_vaga(vaga_nova)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VagaDetalhes(APIView):
+    def get(self, request, id, format=None):
+        vaga = vaga_service.listar_vagas_id(id)
+        serializer = vaga_serializer.VagaSerializer(vaga)
+        return Response(serializer.data, status=status.HTTP_200_OK)
